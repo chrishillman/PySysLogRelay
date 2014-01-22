cbdd734f8b71aef1912424c1e85e941a04f0ba7d
chrishillman <chris.hillman@gmail.com>
2014-01-22 15:26:53 -0500

    Create syslogRelay.py
    syslogRelay.py reads settings from an ini file (if the file is not present, the program creates the file with default values).  The syslog messages are logged to a local text file and then relayed to a port and server specified in the ini file.

---
+++ syslogRelay.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import socket
+import logging
+import SocketServer
+content=""
+LISTEN_HOST, LISTEN_PORT, SEND_HOST, SEND_PORT, LOG_FILE = "0.0.0.0", 514, "127.0.0.1", 515, 'syslogfile_proxy.log'
+
+try:
+    with open("syslog.ini") as file:
+        content = file.readlines()
+    for line in content:
+        if (line.split("=")[0] == "LOG_FILE"):
+            LOG_FILE = line.split("=")[1].strip()
+        if (line.split("=")[0] == "LISTEN_HOST"):
+            LISTEN_HOST = line.split("=")[1].strip()
+        if (line.split("=")[0] == "LISTEN_PORT"):
+            LISTEN_PORT = int(line.split("=")[1])
+        if (line.split("=")[0] == "SEND_HOST"):
+            SEND_HOST = line.split("=")[1].strip()
+        if (line.split("=")[0] == "SEND_PORT"):
+            SEND_PORT = int(line.split("=")[1])
+except (IOError, SystemExit):
+    file = open("syslog.ini", 'w')
+    file.write("LOG_FILE="+LOG_FILE + '\n')
+    file.write("LISTEN_HOST="+LISTEN_HOST+ '\n')
+    file.write("LISTEN_PORT="+str(LISTEN_PORT)+ '\n')
+    file.write("SEND_HOST="+SEND_HOST+ '\n')
+    file.write("SEND_PORT="+str(SEND_PORT)+ '\n')
+
+print("Configuration:" + '\n')
+print("LOG_FILE="+LOG_FILE + '\n')
+print("LISTEN_HOST="+LISTEN_HOST+ '\n')
+print("LISTEN_PORT="+str(LISTEN_PORT)+ '\n')
+print("SEND_HOST="+SEND_HOST+ '\n')
+print("SEND_PORT="+str(SEND_PORT)+ '\n')
+print('\n'+" Display Logged Data: "+'\n')
+
+logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
+ 
+class SyslogUDPHandler(SocketServer.BaseRequestHandler):
+ 
+    def handle(self):
+        data = bytes.decode(self.request[0].strip())
+        print( "%s : " % self.client_address[0], str(data))
+        logging.info(str(data))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(data, (SEND_HOST, SEND_PORT))
+ 
+if __name__ == "__main__":
+	try:
+		server = SocketServer.UDPServer((LISTEN_HOST,LISTEN_PORT), SyslogUDPHandler)
+		server.serve_forever(poll_interval=0.25)
+	except (IOError, SystemExit):
+		raise
+	except KeyboardInterrupt:
+		print ("Crtl+C Pressed. Shutting down.")
